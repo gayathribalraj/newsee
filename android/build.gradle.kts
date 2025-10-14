5f95a1bd75938cf6a89b0c18018dbce75ff1e8eb
gayathribalraj <gayathri.balraj@sysarcinfomatix.com>
2025-10-14 12:08:22 +0530

    test commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.5.2'
+        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25'
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,17 +16,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+rootProject.buildDir = "../build"
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
 }
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,4 +1,9 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.3.0"
+        id("org.jetbrains.kotlin.android") version "1.9.10"
+    }
+
     val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
@@ -15,11 +20,3 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-}
-
-include(":app")
